632e7c08bd51172b94ff8b4de4f9f5e0f4ef55a0
Ileumassamuel <53974949+Ileumassamuel@users.noreply.github.com>
2022-02-14 15:24:18 -0500

    Removed bug causing leaf namespace

--- app/apis/__init__.py
+++ app/apis/__init__.py
@@ -3,7 +3,6 @@ from flask import Blueprint
 
 from .mapNs import api as mapNamespace
 from .treemapNs import api as treemapNamespace
-from .leafNs import api as leafNamespace
 
 apiBlueprint = Blueprint("api", __name__, url_prefix="/api/v1")
 
@@ -11,4 +10,3 @@ api = Api(apiBlueprint, title="API", description="Main routes.")
 
 api.add_namespace(mapNamespace)
 api.add_namespace(treemapNamespace)
-# api.add_namespace(leafNamespace)
